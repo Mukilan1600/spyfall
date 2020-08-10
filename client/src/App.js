bab7e849066655cb384ac6dba61ec27c32b4e7af
Mukilan1600 <ak-muki@hotmail.com>
2020-08-10 12:59:28 +0530

    Spinner

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
@@ -7,21 +7,35 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import { Provider } from "react-redux";
 import Store from "./redux/Store";
+import { Spinner } from "reactstrap";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Provider store={Store}>
-        <div className="App">
-          <Navbar />
-          <Switch>
-            <Route path="/room/:id" component={Chat} />
-            <Route path="/" component={Home} />
-          </Switch>
-        </div>
-      </Provider>
-    </BrowserRouter>
-  );
+class App extends Component {
+  state = {
+    loading: true,
+  };
+
+  componentDidMount() {
+    this.setState({ loading: false });
+  }
+
+  render() {
+    const { loading } = this.state;
+    return loading ? (
+      <Spinner style={{ marginTop: 25 + "%", marginLeft: 50 + "%" }} />
+    ) : (
+      <BrowserRouter>
+        <Provider store={Store}>
+          <div className="App">
+            <Navbar />
+            <Switch>
+              <Route path="/room/:id" component={Chat} />
+              <Route path="/" component={Home} />
+            </Switch>
+          </div>
+        </Provider>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
